6ca267fe4a812f9eb900cde00158ef58c8e3f135
LauraWK <wilkenschildt10@gmail.com>
2017-11-06 16:32:38 +0100

    Login startet

---
+++ Buisnesslogic/Login.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace Buisnesslogic
+{
+    public class Login
+    {
+        public string _medarbejderID;
+        public string _password;
+        
+
+        public void get_Login(string medarbejderID, string password)
+        {
+            _medarbejderID = medarbejderID;
+            _password = password;
+
+
+        }
+    }
+}
---
+++ PresentationLogic/LogInForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLogic
+{
+    public partial class LogInForm : Form
+    {
+        public LogInForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_button_LogInForm_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
