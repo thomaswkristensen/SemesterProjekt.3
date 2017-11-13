60d3e10cb965cf2f171bfaff745f4c2803b92cdd
daniel-noerbygaard <daniel-noerbygaard@outlook.dk>
2017-11-13 22:46:41 +0100

    Oprettelse af database-handler og interface

---
+++ DataAccessLogic/DataBaseHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DataAccessLogic
+{
+    class DataBaseHandler : IDataBaseHandler
+    {
+        public void OpenConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Patient_DTO ExecutePatientInfoQuery(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HP_DTO ExecuteHPInfoQuery(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
